92d981010ad928e53fea0aa82948144f53013786
eljoserass <jose-antonio.rodriguez-assalone@epitech.eu>
2024-06-01 06:35:53 +0100

    [FIX] removed file content from chat

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,11 +12,8 @@ export function FooterText({ className, ...props }: React.ComponentProps<'p'>) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{' '}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
-      </ExternalLink>
+      Made with ❤️ in colab with {' '}
+      <ExternalLink href="https://www.extia-group.com/es-en">Extia</ExternalLink> 
       .
     </p>
   )
--- pdf-worker-loader.ts
+++
@@ -1,6 +0,0 @@
-import * as pdfjsLib from 'pdfjs-dist/build/pdf';
-
-// Use the worker source from a CDN
-pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/react/18.3.1/cjs/react.production.min.js";
-
-export default pdfjsLib;
